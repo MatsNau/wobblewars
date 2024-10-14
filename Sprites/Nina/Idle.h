ea915e2fd13a616e3c9b1ffa427e1ff66ff84f19
MatsNau <163524842+MatsNau@users.noreply.github.com>
2024-10-14 10:48:24 +0200

    added nds_nflib and the sprites to repo

---
+++ Sprites/Nina/Idle.h
@@ -0,0 +1,28 @@
+
+//{{BLOCK(Idle)
+
+//======================================================================
+//
+//	Idle, 32x128@8, 
+//	+ palette 256 entries, not compressed
+//	+ 64 tiles not compressed
+//	Total size: 512 + 4096 = 4608
+//
+//	Time-stamp: 2024-10-10, 23:38:37
+//	Exported by Cearn's GBA Image Transmogrifier, v0.8.6
+//	( http://www.coranac.com/projects/#grit )
+//
+//======================================================================
+
+#ifndef GRIT_IDLE_H
+#define GRIT_IDLE_H
+
+#define IdleTilesLen 4096
+extern const unsigned short IdleTiles[2048];
+
+#define IdlePalLen 512
+extern const unsigned short IdlePal[256];
+
+#endif // GRIT_IDLE_H
+
+//}}BLOCK(Idle)
